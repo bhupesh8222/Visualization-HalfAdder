af6ff042b88c2baa1fad5dd295b3dc67dc0d7142
Bhupesh <bhupeshinfantasy@gmail.com>
2020-09-16 22:10:42 +0530

    another minor changes

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ let data = {
     inputA: [1, 0, 1, 0, 1],
     inputB: [1, 1, 1, 1, 0],
     sum: [0, 1, 0, 1, 1],
-    carry: [1, 0, 1, 0, 0]
+    carry: [1, 0, 1, 0, 0],
+    time: ["0-100", "100-200", "200-300", "300-400", "400-500"]
 }
+
+//visualize this data in the dom
