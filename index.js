2f0916db0c069bbb113fd1df49ddb0465f4f2a5e
Aman Singal <amansingal6@gmail.com>
2020-09-19 12:31:49 +0530

    Added JS

--- index.js
+++ index.js
@@ -1,9 +1,76 @@
-let data = {
-    inputA: [1, 0, 1, 0, 1],
-    inputB: [1, 1, 1, 1, 0],
-    sum: [0, 1, 0, 1, 1],
-    carry: [1, 0, 1, 0, 0],
-    time: ["0-100", "100-200", "200-300", "300-400", "400-500"]
-}
+$(document).ready(function(){
+   let index = -1
+    let input1Handler = $('#input1')
+    let input2Handler = $('#input2')
+    let sumHandler = $('#sum')
+    let carryHandler = $('#carry')
+    let prevHandler = $('#prev')
+    let nextHandler = $('#next')
+    
+    
+    
+    let data = [{inputA: 0, inputB: 1, sum: 0, carry: 1,timeStart:0,timeEnd:100},
+                {inputA: 1, inputB: 0, sum: 0, carry: 1,timeStart:100,timeEnd:200},
+                {inputA: 1, inputB: 1, sum: 1, carry: 0,timeStart:200,timeEnd:300},]
+    
+    
 
-//visualize this data in the dom
+    prevHandler.attr("disabled", true);
+    
+    nextHandler.click(function(){
+
+        if(index < data.length - 1){
+            index = index + 1;
+            visualHandler(index)
+            prevHandler.attr("disabled", false);
+            if( index === data.length - 1){
+                nextHandler.attr("disabled", true);
+            }
+        }
+        
+    })
+
+    prevHandler.click(function(){
+        if(index > 0){
+            index = index - 1;
+            visualHandler(index)
+            nextHandler.attr("disabled", false);
+            if( index <= 0){
+                prevHandler.attr("disabled", true);
+            }
+        }
+    })
+
+    function visualHandler(index){
+
+        // For Input 1
+        if(data[index].inputA === 0){
+            input1Handler.css('background' , 'red')
+        }else{
+            input1Handler.css('background' , 'green')
+        }
+
+        // For Input 2 
+        if(data[index].inputB === 0){
+            input2Handler.css('background' , 'red')
+        }else{
+            input2Handler.css('background' , 'green')
+        }
+
+        // For Sum
+        if(data[index].sum === 0){
+            sumHandler.css('background' , 'red')
+        }else{
+            sumHandler.css('background' , 'green')
+        }
+
+        // For Carry
+        if(data[index].carry === 0){
+            carryHandler.css('background' , 'red')
+        }else{
+            carryHandler.css('background' , 'green')
+        }
+        
+    }
+    
+  });
